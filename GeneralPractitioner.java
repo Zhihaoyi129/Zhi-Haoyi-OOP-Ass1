2c7c8daf78200bf5b723325dbab46fc6cb73cd01
Zhihaoyi129 <h.zhi.13@student.scu.edu.au>
2024-11-20 14:54:03 +0800

    Update GeneralPractitioner.java
    Provides the characteristics of the doctor

--- GeneralPractitioner.java
+++ GeneralPractitioner.java
@@ -1,3 +1,27 @@
-public class GeneralPractitioner {
-    
+public class GeneralPractitioner extends HealthProfessional {
+    private String specialty;
+
+    public GeneralPractitioner() {
+        super();
+        this.specialty = "";
+    }
+
+    public GeneralPractitioner(int id, String name, String info, String specialty) {
+        super(id, name, info);
+        this.specialty = specialty;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Specialty: " + specialty);
+    }
 }
