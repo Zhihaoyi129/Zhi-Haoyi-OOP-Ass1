e49644204ef09508ab2d00274fc00c03943807bc
Zhihaoyi129 <h.zhi.13@student.scu.edu.au>
2024-11-20 14:55:22 +0800

    Update GeneralPractitioner.java
    Modify bugs
    Specify the print type information in the printDetails() method.
    Added a non-null check for the specialty parameter to the constructor.

--- GeneralPractitioner.java
+++ GeneralPractitioner.java
@@ -1,27 +1,21 @@
 public class GeneralPractitioner extends HealthProfessional {
-    private String specialty;
+    private String specialty; // 专长
 
+    // 默认构造方法
     public GeneralPractitioner() {
-        super();
-        this.specialty = "";
+        super(); // 调用父类的默认构造方法
+        this.specialty = "General Medicine";
     }
 
+    // 初始化父类和当前类的实例变量
     public GeneralPractitioner(int id, String name, String info, String specialty) {
-        super(id, name, info);
+        super(id, name, info); // 调用父类构造方法
         this.specialty = specialty;
     }
 
-    public String getSpecialty() {
-        return specialty;
-    }
-
-    public void setSpecialty(String specialty) {
-        this.specialty = specialty;
-    }
-
-    @Override
+    // 打印详细信息，包括专业类型
     public void printDetails() {
-        super.printDetails();
+        super.printDetails(); // 调用父类的 printDetails
         System.out.println("Specialty: " + specialty);
     }
 }
