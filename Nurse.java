061b5c4eb2f5493754bfc4cf78c84e1fa6890d67
Zhihaoyi129 <h.zhi.13@student.scu.edu.au>
2024-11-20 14:57:59 +0800

    Update Nurse.java
    Adds nurse-specific attributes and behaviors.

--- Nurse.java
+++ Nurse.java
@@ -1,3 +1,22 @@
-public class Nurse {
-    
+// Nurse.java
+public class Nurse extends HealthProfessional {
+    private String department; // 所属科室
+
+    // 默认构造方法
+    public Nurse() {
+        super();
+        this.department = "General Nursing";
+    }
+
+    // 初始化父类和当前类的实例变量
+    public Nurse(int id, String name, String info, String department) {
+        super(id, name, info);
+        this.department = department;
+    }
+
+    // 打印详细信息，包括科室
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Department: " + department);
+    }
 }
