73176fb79e179ebba0e02eb85bc257d45bd9bc3c
Zhihaoyi129 <h.zhi.13@student.scu.edu.au>
2024-11-20 14:57:08 +0800

    Update HealthProfessional.java
    As a parent class for all health professionals.
    Contains common properties and methods.

--- HealthProfessional.java
+++ HealthProfessional.java
@@ -1,3 +1,27 @@
+// HealthProfessional.java
 public class HealthProfessional {
-    
+    private int id;
+    private String name;
+    private String info;  // 其他描述信息
+
+    // 默认构造方法
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "Unknown";
+        this.info = "General info";
+    }
+
+    // 构造方法初始化所有实例变量
+    public HealthProfessional(int id, String name, String info) {
+        this.id = id;
+        this.name = name;
+        this.info = info;
+    }
+
+    // 打印健康专业人员的详情
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Info: " + info);
+    }
 }
